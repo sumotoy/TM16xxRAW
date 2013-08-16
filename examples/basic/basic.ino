02ba9287e50ab1f30c0ce94fbac550f4a4884a51
max mc costa <sumotoy@gmail.com>
2013-08-17 00:37:14 +0300

    Update basic.ino

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,17 +1,23 @@
 #include <TM16xxRAW.h>
 
+
 //data_pin,clock_pin,strobe_pin
 TM16xxRAW tm(3,4,5);
 
 
 void setup() {
-//optional:brightness(0:7),displayOn(0=off,1=on)
- tm.begin();
-//column,row,state(0,1)
- tm.setLed(0,1,1);
+  //optional:brightness(0:7),displayOn(0=off,1=on)
+  tm.begin();
+  //column,row,state(0,1)
+  tm.setLed(0,1,1);
 }
 
 void loop() {
+  randomLed2();
+}
+
+//this use column,row led addressing
+void randomLed1(){
   byte col = random(0,8);
   byte row = random(0,8);
   byte val = 0;
@@ -23,5 +29,23 @@ void loop() {
   }
   tm.setLed(col,row,val);
   delay(5);
-  
+}
+
+//this use just led addressing.
+/*
+using 8x8 matrix you have 64 led addressable.
+you can use any number from 0 to 63, the led are addressed as colums
+so led 0 to 7 will be in the column 0 and so on.
+*/
+void randomLed2(){
+  byte led = random(0,63);
+  byte val = 0;
+  if (tm.getLed(led) == 1){
+    val = 0;
+  } 
+  else {
+    val = 1;
+  }
+  tm.setLed(led,val);  
+  delay(5);
 }
